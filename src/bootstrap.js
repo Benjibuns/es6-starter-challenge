f891f218f264bd89f355f89302c4ad822f60b083
Ben <benriveracoding@gmail.com>
2021-02-02 18:49:03 -0700

    created first project using imports and exports

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import classChallenge, {greeting, arrayTotal} from "./classChallenge"
+
+console.log(classChallenge())
+console.log(greeting)
+console.log(arrayTotal(4,3,2,1))
---
+++ src/classChallenge.js
@@ -0,0 +1,9 @@
+export default function() {
+    return "<div>classChallenge</div>"
+}
+
+export const greeting = "welcome"
+
+export function arrayTotal(...numList) {
+    return numList.reduce((total, num) => total + num)
+}
